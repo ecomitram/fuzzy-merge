654088ce59e0cd75865fc90b9546d86734eead44
Shyam <shyam@readybytes.in>
2023-08-28 05:43:59 +0000

    fixes

--- index.js
+++ index.js
@@ -22,11 +22,23 @@ function cleanName(name, city) {
   //lowercase
   name = name.toLowerCase();
 
-  //remove cioty names
-  name = name.replace(city.toLowerCase(), "");
+  name = name.replaceAll('-', ' ')
+  name = name.replaceAll('.', ' ')
+  name = name.replaceAll('"', '')
+  name = name.replaceAll(',', '')
+
+  //remove city names
+  name = name.replaceAll(city.toLowerCase(), "");
+
+  //remove school
+  name = name.replaceAll('school', "");
 
   //remove non alpha-numeric, with space
   name = name.replace("/[^a-z0-9 ]/g", " ");
+
+  name = name.trim();
+
+
 
   return name;
 }
@@ -41,23 +53,25 @@ function mergeRecords(csvData) {
   //empty log file
   fs.writeFileSync("output/matches.log", "");
 
-  const pincodeFuzzymap = {};
+  const cityFuzzyMap = {};
   csvData.forEach((row, index) => {
-    const [nameWithPincode, count, origName, pincode, city] = row;
+    const [nameWithCity, count, origName, pincode, city, state] = row;
 
     const name = cleanName(origName,city);
-    //use seperate fuzzyset for every pincode
-    if(pincodeFuzzymap[pincode] === undefined){
-      pincodeFuzzymap[pincode] = FuzzySet();
+    //use seperate fuzzyset for every city
+    let fuzzyMapKey = `${city}:${state}`;
+    if (cityFuzzyMap[fuzzyMapKey] === undefined) {
+      cityFuzzyMap[fuzzyMapKey] = FuzzySet();
     }
-    const fuzzyMap = pincodeFuzzymap[pincode];
+    const fuzzyMap = cityFuzzyMap[fuzzyMapKey];
 
     if (index && index % 10000 == 0) {
       console.log(
         "Processed:",
         index,
-        "Normalized:",
-        Object.values(finalData).length
+        "  Normalized: ",
+        Object.values(finalData).length,
+        "  Percentage:", 100 * Object.values(finalData).length / index
       );
     }
     // Check if there is a fuzzy match for the name
@@ -93,6 +107,9 @@ function mergeRecords(csvData) {
 // Read the CSV file
 console.time("Execution Time");
 
+//_id,registration_count,name,pincode,city
+// fs.writeFileSync("output/out.csv", "_id,registration_count,name,pincode,city\n");
+
 fs.readFile("input/nspc.csv", "utf8", (err, data) => {
   if (err) {
     console.error(err);
@@ -107,15 +124,44 @@ fs.readFile("input/nspc.csv", "utf8", (err, data) => {
     }
 
     mergeRecords(csvData);
+    console.timeEnd("Execution Time");
     fs.writeFileSync(
-      "output/out.csv",
+      `output/out.csv`,
+      "id,registration_count,name,city,state\n" +
       Object.values(finalData)
-        .map((record) => '"'+Object.values(record).join('","')+'"')
+        .map(function (record, id) {
+
+          let [nameWithCity, registration_count, origName, pincode, city, state] = record;
+          // console.log('record: ', record);
+          origName = origName.replaceAll('"', '').replaceAll(',', ' ').trim();
+          city = city.replaceAll('"', '').replaceAll(',', ' ').trim();
+          registration_count = parseInt(registration_count) || 0;
+          state = state.replaceAll('state:', '').replaceAll('(', '').replaceAll(')', '');
+          return `${id + 1}, ${registration_count},${origName},${city},${state}`;
+        })
         .join("\n")
     );
 
-    console.timeEnd("Execution Time");
+
   });
   
 
 });
+
+
+/*
+
+mlr --csv sort -n  registration_count output/out.csv > output/out-sorted.csv
+
+scp output/out.csv root@139.59.21.157:/var/lib/mysql-files/nspcout.csv
+
+
+LOAD DATA INFILE '/var/lib/mysql-files/nspcout.csv'
+INTO TABLE nspc_institutes
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+
+ */
