797ceae44cbc6603a1bb642ea8b4943329799903
Shyam <shyam@readybytes.in>
2024-08-14 12:26:41 +0000

    refactor: Remove unused district "khandwa" and add new district "East Nimar"

--- mapper.js
+++ mapper.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const csvParse = require("csv-parse");
 const FuzzySet = require("fuzzyset");
+const { exit } = require("process");
 
 // const pincodeData = require('./pincodes.json');
 
@@ -32,7 +33,6 @@ function fuzzymatch(name, map) {
 
 const statesDate = require('./states.json');
 const statesMap = [];
-
 for (let index = 0; index < statesDate.length; index++) {
     const element = statesDate[index];
     statesMap[cleanName(element.id)] = element.id;
@@ -40,7 +40,6 @@ for (let index = 0; index < statesDate.length; index++) {
 
 const districtsData = require('./districts.json');
 const districtsMap = [];
-
 for (let index = 0; index < districtsData.length; index++) {
     const element = districtsData[index];
     districtsMap[cleanName(element.id)] = {
@@ -51,8 +50,36 @@ for (let index = 0; index < districtsData.length; index++) {
     districtFuzzyMap.add(cleanName(element.id));
 }
 
+//process prant to kshetra mapping
+const prantMap = [];
 
+fs.readFile("./kshetra.csv", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      
+    }
+  
+    // Parse the CSV data
+    csvParse(data, { delimiter: "," }, (err, csvData) => {
+      if (err) {
+        console.error(err);
+        exit(1);
+      }
 
+        matched = 0;
+        notMatched = 0;
+        csvData.forEach((row, index) => {
+            let [prant,kshetra] = row;
+            prant = cleanName(prant);
+
+            // append prant 
+            prantMap[prant] = kshetra;
+        });
+        console.log('prantMap: ', prantMap);
+    });
+});
+
+// process prant to district mapping
 fs.readFile("./prants.csv", "utf8", (err, data) => {
     if (err) {
       console.error(err);
@@ -66,38 +93,20 @@ fs.readFile("./prants.csv", "utf8", (err, data) => {
         return;
       }
 
-      matched = 0;
-    notMatched = 0;
-    fuzzyMatched = 0 ;
-      csvData.forEach((row, index) => {
-        let [state, district, prant] = row;
+        csvData.forEach((row, index) => {
+            let [state, district, prant] = row;
+            district = cleanName(district);
 
-        district = cleanName(district);
-
-        
-        if (!districtsMap[district]) {
-            if(fdistrict=fuzzymatch(district, districtFuzzyMap)){
-                console.log(`Fuzzy Matched:  ./prants.csv:${index+1}`, index,' District: ', district, '|' ,fdistrict);
-                fuzzyMatched++;
-            }else{
-                console.log(` ./prants.csv:${index+1}`, 'Not Matched:  District: ', district, ' State:', state);
-                notMatched++;
+            if(!prantMap[cleanName(prant)]){
+                console.log('Prant not found: ', prant);
+                exit(1);
             }
-        }else{
-            matched++;
-            //  console.log('Matched District Name: ', district);
-        }
-        
-
-        // if (!statesMap[state]) {
-        //     console.log('Not Matched State Name: ', state);
-        // }
-
-      });
-      console.log('Matched: ', matched, 'Not Matched: ', notMatched);
+            // append prant 
+            districtsMap[district] = {...districtsMap[district], 'prant': prant, 'kshetra': prantMap[cleanName(prant)] };
+        });
     });
+    console.log('districtsMap: ', districtsMap);
 });
-
 // 
 
 // for (let index = 0; index < districtsMap.length; index++) {
