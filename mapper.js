8c9e1e0f90c04ccf9ee478e91ffa111e796e7de4
Shyam <shyam@readybytes.in>
2024-08-14 13:15:17 +0000

    refactor: Update district mapping for better matching and limit to same pincode

--- mapper.js
+++ mapper.js
@@ -3,7 +3,6 @@ const csvParse = require("csv-parse");
 const FuzzySet = require("fuzzyset");
 const { exit } = require("process");
 
-// const pincodeData = require('./pincodes.json');
 
 function cleanName(name) {
     name = name.trim();
@@ -26,20 +25,13 @@ function fuzzymatch(name, map) {
     matches = map.get(name, null, 0.75);
     if (matches) {
         return matches[0][1];
-    }else{
+    } else {
         return null;
     }
 }
 
-const statesDate = require('./states.json');
-const statesMap = [];
-for (let index = 0; index < statesDate.length; index++) {
-    const element = statesDate[index];
-    statesMap[cleanName(element.id)] = element.id;
-}
-
 const districtsData = require('./districts.json');
-const districtsMap = [];
+const districtsMap = {};
 for (let index = 0; index < districtsData.length; index++) {
     const element = districtsData[index];
     districtsMap[cleanName(element.id)] = {
@@ -49,67 +41,68 @@ for (let index = 0; index < districtsData.length; index++) {
 
     districtFuzzyMap.add(cleanName(element.id));
 }
+// console.log('districtsMap: ', districtsMap.length);
 
 //process prant to kshetra mapping
-const prantMap = [];
-
+const prantMap = {};
 fs.readFile("./kshetra.csv", "utf8", (err, data) => {
     if (err) {
-      console.error(err);
-      
+        console.error(err);
+
     }
-  
+
     // Parse the CSV data
     csvParse(data, { delimiter: "," }, (err, csvData) => {
-      if (err) {
-        console.error(err);
-        exit(1);
-      }
+        if (err) {
+            console.error(err);
+            exit(1);
+        }
 
         matched = 0;
         notMatched = 0;
-        csvData.forEach((row, index) => {
-            let [prant,kshetra] = row;
+        for (const row of csvData) {
+            let [prant, kshetra] = row;
             prant = cleanName(prant);
 
             // append prant 
             prantMap[prant] = kshetra;
-        });
-        console.log('prantMap: ', prantMap);
-    });
-});
+            console.log('prant: ', prant, 'kshetra: ', kshetra);
+        };
+        console.log('prantMap Length: ', prantMap.length);
 
-// process prant to district mapping
-fs.readFile("./prants.csv", "utf8", (err, data) => {
-    if (err) {
-      console.error(err);
-      exit(1);
-    }
-  
-    // Parse the CSV data
-    csvParse(data, { delimiter: "," }, (err, csvData) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-
-        csvData.forEach((row, index) => {
-            let [state, district, prant] = row;
-            district = cleanName(district);
-
-            if(!prantMap[cleanName(prant)]){
-                console.log('Prant not found: ', prant);
+        // process prant to district mapping
+        fs.readFile("./prants.csv", "utf8", (err, data) => {
+            if (err) {
+                console.error(err);
                 exit(1);
             }
-            // append prant 
-            districtsMap[district] = {...districtsMap[district], 'prant': prant, 'kshetra': prantMap[cleanName(prant)] };
+
+            // Parse the CSV data
+            csvParse(data, { delimiter: "," }, (err, csvData) => {
+                if (err) {
+                    console.error(err);
+                    return;
+                }
+
+                for (const row of csvData) {
+                    let [state, district, prant] = row;
+                    district = cleanName(district);
+
+                    if (!prantMap[cleanName(prant)]) {
+                        console.log('Prant not found: ', prant);
+                        console.log('prantMap: ', prantMap);
+
+                    }
+                    // append prant 
+                    districtsMap[district] = { ...districtsMap[district], 'prant': prant, 'kshetra': prantMap[cleanName(prant)] };
+                }
+                console.log('districtsMap Length: ', districtsMap.length);
+
+                console.log('Exporting...');
+                fs.writeFileSync(`./prants.json`, JSON.stringify(prantMap, null, 2));
+                fs.writeFileSync(`./mapper.json`, JSON.stringify(districtsMap, null, 2));
+                console.log('Done');
+            });
         });
     });
-    console.log('districtsMap: ', districtsMap);
 });
-// 
-
-// for (let index = 0; index < districtsMap.length; index++) {
-//     const element = districtsMap[index];
-//     console.log(element);
-// }
